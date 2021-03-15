1a7176fb64edd406b835b7ecffe201af83d893e6
Jeremy Mullis <jeremy.mullis@gmail.com>
2021-03-15 15:42:05 -0500

    add template for password protected pages

--- page.php
+++ page.php
@@ -4,4 +4,17 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
-Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
+
+if (post_password_required($timber_post->ID)) {
+	Timber::render('single-password.twig', $context);
+} else {
+	Timber::render(
+		[
+			'page-' . $timber_post->ID . '.twig',
+			'page-' . $timber_post->post_type . '.twig',
+			'page-' . $timber_post->slug . '.twig',
+			'page.twig',
+		],
+		$context
+	);
+}
